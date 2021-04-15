6960ef99a8c82db908cef0308a5131d4845d9b02
astokrockiy <78504978+astokrockiy@users.noreply.github.com>
2021-04-15 19:52:23 +0300

    Add files via upload

---
+++ first-temlate/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    require('./vendor/slick.min');
+
+    
+    require('./components/js-connect');
+    require('./components/header');
+});
